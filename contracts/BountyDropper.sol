0eef46f861111ca9a4187233cf74f4dab31b4dd9
JBT <moskvant.ai@gmail.com>
2018-08-24 13:31:26 +0300

    improving tests

--- contracts/BountyDropper.sol
+++ contracts/BountyDropper.sol
@@ -4,13 +4,15 @@ import "./ERC20Token.sol";
 contract BountyDropper {
 
     address public owner;
+    uint public added1;
+    uint public added2;
 
     struct Stake {
         address[] hunterList;
         mapping(address => uint) stakeMap;
         uint stakeToTokens;
     }
-    mapping(address => Stake) public stakes;
+    mapping(address => mapping(uint => Stake)) public stakes;
 
     modifier onlyOwner() {
         require(msg.sender == owner);
@@ -21,23 +23,44 @@ contract BountyDropper {
         owner = msg.sender;
     }
 
-    function addStake(address _tokenAddress, uint _stake, address _bountyHunterAddress) public onlyOwner {
-        require(_stake != 0);
-        if (stakes[_tokenAddress].stakeMap[_bountyHunterAddress] == 0) {
-            stakes[_tokenAddress].hunterList.push(_bountyHunterAddress);
+    function addStakes(address[] _tokenAddresses, uint[] _stakes, address[] _bountyHunterAddresses, uint[] _campaigns) public onlyOwner {
+        require(_tokenAddresses.length == _stakes.length);
+        require(_stakes.length == _bountyHunterAddresses.length);
+        require(_bountyHunterAddresses.length == _campaigns.length);
+
+        for ( uint i=0; i<_stakes.length; i++) {
+            if (stakes[_tokenAddresses[i]][_campaigns[i]].stakeMap[_bountyHunterAddresses[i]] > 0) {
+                stakes[_tokenAddresses[i]][_campaigns[i]].stakeMap[_bountyHunterAddresses[i]] += _stakes[i];
+                added1 += 1;
+            } else {
+                stakes[_tokenAddresses[i]][_campaigns[i]].hunterList.push(_bountyHunterAddresses[i]);
+                stakes[_tokenAddresses[i]][_campaigns[i]].stakeMap[_bountyHunterAddresses[i]] = _stakes[i];
+                added2 += 1;
+            }
+
         }
-        stakes[_tokenAddress].stakeMap[_bountyHunterAddress] += _stake;
     }
 
-    function distributeStakes(address _tokenAddress, uint _stakeToTokens) public onlyOwner {
+    function addStake(address _tokenAddress, uint _stake, address _bountyHunterAddress, uint _campaign) public onlyOwner {
+        require(_stake != 0);
+        if (stakes[_tokenAddress][_campaign].stakeMap[_bountyHunterAddress] == 0) {
+            stakes[_tokenAddress][_campaign].hunterList.push(_bountyHunterAddress);
+        }
+        stakes[_tokenAddress][_campaign].stakeMap[_bountyHunterAddress] += _stake;
+    }
+    function huntinGames(address _tokenAddress, uint _campaign) public onlyOwner returns(uint) {
+        return stakes[_tokenAddress][_campaign].hunterList.length;
+    }
 
+    function distributeStakes(address _tokenAddress, uint _campaign ,uint _stakeToTokens) public onlyOwner {
+        require(stakes[_tokenAddress][_campaign].hunterList.length > 0);
         Token token = Token(_tokenAddress);
 
-        stakes[_tokenAddress].stakeToTokens = _stakeToTokens;
+        stakes[_tokenAddress][_campaign].stakeToTokens = _stakeToTokens;
 
-        for ( uint i = 0; i < stakes[_tokenAddress].hunterList.length; i++ ) {
-            address _bountyHunter = stakes[_tokenAddress].hunterList[i];
-            uint _tokenAmount = stakes[_tokenAddress].stakeMap[_bountyHunter] * _stakeToTokens;
+        for ( uint i = 0; i < stakes[_tokenAddress][_campaign].hunterList.length; i++ ) {
+            address _bountyHunter = stakes[_tokenAddress][_campaign].hunterList[i];
+            uint _tokenAmount = stakes[_tokenAddress][_campaign].stakeMap[_bountyHunter] * _stakeToTokens;
             token.transfer(_bountyHunter, _tokenAmount);
         }
     }
--- test/test1.js
+++ test/test1.js
@@ -12,7 +12,7 @@ bountyHunter1 = accounts[4]
 bountyHunter2 = accounts[5]
 bountyHunter3 = accounts[6]
 
-tokenAmount = 500
+tokenAmount = 50000
 
 contract('BountyDropper', function(accounts) {
 
@@ -43,33 +43,52 @@ contract('BountyDropper', function(accounts) {
         assert.equal(balance2.valueOf(), tokenAmount*500, "Tokens have not been minted to the ICOContract");
         assert.equal(balance3.valueOf(), tokenAmount*500, "Tokens have not been minted to the ICOContract");
     });
-    it('Add 500 stakes in ICO-1 for BH-1', async function() {
-        await bountyDropper.addStake(token1.address,500,bountyHunter1)
+    it('addes stakes', async function() {
+        // _tokenAddresses = [token1.address,token1.address,token1.address,token1.address,token2.address,token2.address,token2.address,token2.address,token3.address,token3.address,token3.address,token3.address]
+        // _campaigns =      [0,0,1,1,0,0,1,2,0,1,2,3]
+        // _stakes = [10,20,11,1,15,2,1,2,5,10,20,30]
+        // _bountyHunterAddresses = [bountyHunter1,bountyHunter2,bountyHunter3,bountyHunter1,bountyHunter2,bountyHunter3,bountyHunter1,bountyHunter2,bountyHunter3,bountyHunter1,bountyHunter2,bountyHunter3]
+
+        _tokenAddresses = ['0xb3fe04fe73d672001f2f31549592911c9e4539e6']
+        _campaigns =      [0]
+        _stakes = [5]
+        _bountyHunterAddresses = ['0xb3fe04fe73d672001f2f31549592911c9e4539e6']
+        console.log(_tokenAddresses.length)
+        console.log(_campaigns.length)
+        console.log(_stakes.length)
+        console.log(_bountyHunterAddresses.length)
+        await bountyDropper.addStakes(_tokenAddresses, _campaigns,_stakes,  _bountyHunterAddresses)
     })
-    it('Add 100 stakes in ICO-1 for BH-2', async function() {
-        await bountyDropper.addStake(token1.address,100,bountyHunter2)
+    it('added 1 and 2', async function() {
+        ad1 = await bountyDropper.added1()
+        ad2 = await bountyDropper.added2()
+        console.log(ad1.valueOf())
+        console.log(ad2.valueOf())
     })
-    it('Add 1000 stakes in ICO-1 for BH-3', async function() {
-        await bountyDropper.addStake(token1.address,1000,bountyHunter3)
-    })
-    it('Add 500 stakes in ICO-2 for BH-1', async function() {
-        await bountyDropper.addStake(token2.address,500,bountyHunter1)
-    })
-    it('Add 50 stakes in ICO-2 for BH-2', async function() {
-        await bountyDropper.addStake(token2.address,50,bountyHunter2)
-    })
-    it('Add 300 stakes in ICO-2 for BH-3', async function() {
-        await bountyDropper.addStake(token2.address,300,bountyHunter3)
-    })
-    it('Add 500 stakes in ICO-3 for BH-1', async function() {
-        await bountyDropper.addStake(token3.address,500,bountyHunter1)
-    })
-    it('Add 500 stakes in ICO-3 for BH-2', async function() {
-        await bountyDropper.addStake(token3.address,1000,bountyHunter2)
-    })
-    it('Add 500 stakes in ICO-3 for BH-3', async function() {
-        await bountyDropper.addStake(token3.address,400,bountyHunter3)
-    })
+    // it('Add 100 stakes in ICO-1 for BH-2', async function() {
+    //     await bountyDropper.addStake(token1.address,100,bountyHunter2)
+    // })
+    // it('Add 1000 stakes in ICO-1 for BH-3', async function() {
+    //     await bountyDropper.addStake(token1.address,1000,bountyHunter3)
+    // })
+    // it('Add 500 stakes in ICO-2 for BH-1', async function() {
+    //     await bountyDropper.addStake(token2.address,500,bountyHunter1)
+    // })
+    // it('Add 50 stakes in ICO-2 for BH-2', async function() {
+    //     await bountyDropper.addStake(token2.address,50,bountyHunter2)
+    // })
+    // it('Add 300 stakes in ICO-2 for BH-3', async function() {
+    //     await bountyDropper.addStake(token2.address,300,bountyHunter3)
+    // })
+    // it('Add 500 stakes in ICO-3 for BH-1', async function() {
+    //     await bountyDropper.addStake(token3.address,500,bountyHunter1)
+    // })
+    // it('Add 500 stakes in ICO-3 for BH-2', async function() {
+    //     await bountyDropper.addStake(token3.address,1000,bountyHunter2)
+    // })
+    // it('Add 500 stakes in ICO-3 for BH-3', async function() {
+    //     await bountyDropper.addStake(token3.address,400,bountyHunter3)
+    // })
     it('Prelimiary Balances', async function() {
         var a1 = await token1.balanceOf(bountyHunter1)
         var a2 = await token1.balanceOf(bountyHunter2)
@@ -101,8 +120,13 @@ contract('BountyDropper', function(accounts) {
         `
         console.log(message)
     })
+
+    it('HUNTING GAMES', async function() {
+        var what = await bountyDropper.huntinGames.call('0xb3fe04fe73d672001f2f31549592911c9e4539e6',0)
+        console.log(what.valueOf())
+    })
     it('Distributes stakes in ICO-1 at rate of 50', async function() {
-        await bountyDropper.distributeStakes(token1.address, 50)
+        await bountyDropper.distributeStakes(token1.address,0,1)
     })
     it('Balances', async function() {
         var a1 = await token1.balanceOf(bountyHunter1)
@@ -135,8 +159,8 @@ contract('BountyDropper', function(accounts) {
         `
         console.log(message)
     })
-    it('Distributes stakes in ICO-2 at rate of 10', async function() {
-        await bountyDropper.distributeStakes(token2.address, 10)
+    it('Distributes stakes in ICO-1 at rate of 50', async function() {
+        await bountyDropper.distributeStakes(token1.address,1,3)
     })
     it('Balances', async function() {
         var a1 = await token1.balanceOf(bountyHunter1)
@@ -169,41 +193,6 @@ contract('BountyDropper', function(accounts) {
         `
         console.log(message)
     })
-    it('Distributes stakes in ICO-2 at rate of 3', async function() {
-        await bountyDropper.distributeStakes(token3.address, 3)
-    })
-    it('Balances', async function() {
-        var a1 = await token1.balanceOf(bountyHunter1)
-        var a2 = await token1.balanceOf(bountyHunter2)
-        var a3 = await token1.balanceOf(bountyHunter3)
-
-        var b1 = await token2.balanceOf(bountyHunter1)
-        var b2 = await token2.balanceOf(bountyHunter2)
-        var b3 = await token2.balanceOf(bountyHunter3)
-
-        var c1 = await token3.balanceOf(bountyHunter1)
-        var c2 = await token3.balanceOf(bountyHunter2)
-        var c3 = await token3.balanceOf(bountyHunter3)
-
-        message = `
-            Token 1 Balances
-            BountyHunter1 : `+a1.valueOf()+`
-            BountyHunter2 : `+a2.valueOf()+`
-            BountyHunter3 : `+a3.valueOf()+`
-            -------------------------
-            Token 2 Balances
-            BountyHunter1 : `+b1.valueOf()+`
-            BountyHunter2 : `+b2.valueOf()+`
-            BountyHunter3 : `+b3.valueOf()+`
-            -------------------------
-            Token 3 Balances
-            BountyHunter1 : `+c1.valueOf()+`
-            BountyHunter2 : `+c2.valueOf()+`
-            BountyHunter3 : `+c3.valueOf()+`
-        `
-        console.log(message)
-    })
-
 
 
 })
